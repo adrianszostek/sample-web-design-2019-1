a43a2a7c013e1497b88fa5e44330953b23f35f6d
adrianszostek <69784002+adrianszostek@users.noreply.github.com>
2022-06-17 21:02:38 +0200

    folders without underscore

---
+++ WebLauncher.js
@@ -0,0 +1,147 @@
+const WebLauncher = `
+	<div><h3>Interestings</h3>
+		<div>
+			<p>Tech news</p>
+			<a href="https://9to5google.com/">9to5google.com</a>
+			<a href="https://www.tablety.pl/aktualnosci/">Tablety.pl</a>
+			<a href="https://www.tabletowo.pl/">Tabletowo.pl</a>
+			<a href="http://brandingmonitor.pl/">BrandingMonitor.pl</a>
+			<a href="http://antyapps.pl/tag/aplikacje/">Antyapps.pl</a>
+			<a href="http://antyweb.pl/">Antyweb.pl</a>
+			<a href="https://www.spidersweb.pl/">SpidersWeb.pl</a>
+			<a href="http://www.conowego.pl/">Conowego.pl</a>
+		</div>
+		<a href="https://getpocket.com/a/queue/">
+			<img src="_launcher/pocket.svg"/>
+			<p>Pocket</p>
+		</a>
+		<a href="https://blog.sagradafamilia.org/en/">
+			<p>Blog Sagrada Família</p>
+		</a>
+		<div>
+			<p>Zdrowa żywność</p>
+			<a href="http://czytamyetykiety.pl/czytamy-etykiety">Czytamy etykiety</a>	
+			<a href="http://czytajsklad.com/najnowsze/">Czytaj skład</a>	
+		</div>
+		<a href="https://www.instagram.com/">
+			<img src="_launcher/instagram.jpg"/>
+			<p>Instagram</p>
+		</a>	
+		<a href="https://news.google.com/my/library?hl=en-US&gl=US&ceid=US%3Aen">
+			<img src="_launcher/news.png"/>
+			<p>Google News</p>
+		</a>
+	</div>
+
+	<div><h3>Multimedia</h3>	
+		<a href="https://www.youtube.com/feed/subscriptions">
+			<img src="_launcher/youtube.svg"/>
+			<p>YouTube</p>
+		</a>
+		<a href="https://photos.google.com/">
+			<img src="_launcher/photos.png"/>
+			<p>Google Photos</p>
+		</a>
+		<a href="http://www.filmweb.pl/premiere">
+			<p>Filmweb.pl</p>
+		</a>
+		<a href="http://www.teleman.pl/">
+			<p>Teleman.pl</p>
+		</a>
+	</div>
+
+	<div><h3>Sport</h3>
+		<div>
+			<img src="_launcher/sofascore.jpg"/>
+			<p>SofaScore</p>
+			<a href="http://www.sofascore.com/pl/ulubione">Ulubione</a>
+			<a href="https://www.sofascore.com/pl/turniej/pilka-nozna/poland/ekstraklasa/202">Ekstraklasa</a>
+		</div>
+		<a href="https://twitter.com/adrian_szostek/lists/sport">
+			<img src="_launcher/twitter.jpg"/>
+			<p>Twitter</p>
+		</a>
+		<a href="https://www.facebook.com/lists/1416790525098293">
+			<img src="_launcher/facebook.jpg"/>
+			<p>Facebook</p>
+		</a>
+		<div>
+			<p>Piłka nożna</p>
+			<a href="https://www.laczynaspilka.pl/reprezentacja-a,5304/artykuly,1.html">LaczyNasPilka.pl</a>
+			<a href="https://ekstraklasa.org/">Ekstraklasa.org</a>	
+			<a href="http://www.stadiony.net/">Stadiony.net</a>
+		</div>
+		<div>
+			<p>Skoki narciarskie</p>
+			<a href="http://www.skijumping.pl/">Skijumping.pl</a>
+			<a href="http://www.skijumping.pl/live/">Live timing</a>
+		</div>
+		<a href="http://www.lajfy.com/">
+			<p>Lajfy.com</p>
+		</a>
+	</div>
+
+	<div><h3>Office desk</h3>
+		<a href="https://drive.google.com/drive/u/0/my-drive">
+			<img src="_launcher/drive.png"/>
+			<p>Google Drive</p>
+		</a>
+		<a href="https://calendar.google.com/calendar/r/month">
+			<img src="_launcher/calendar.png"/>
+			<p>Google Calendar</p>
+		</a>
+		<a href="https://keep.google.com/#home">
+			<img src="_launcher/keep.png"/>
+			<p>Google Keep</p>
+		</a>
+		<a href="https://mail.google.com/mail/u/0/#inbox">
+			<img src="_launcher/gmail.png"/>
+			<p>Gmail</p>
+		</a>
+		<a href="https://trello.com/b/1peYHApV/rw-documents">
+			<img src="_launcher/trello.jpg"/>
+			<p>Trello</p>
+		</a>
+		<a href="https://translate.google.com/m/translate">
+			<img src="_launcher/translate.png"/>
+			<p>Google Translate</p>
+		</a>
+		<a href="https://synonim.net/">
+			<p>Synonim.net</p>
+		</a>
+	</div>
+
+	<div><h3>Planning</h3>
+		<a href="https://www.google.pl/maps?source=tldsi&hl=pl">
+			<img src="_launcher/maps.jpg"/>
+			<p>Google Maps</p>
+		</a>
+		<a href="https://map.airly.eu/pl/#latitude=49.98643&longitude=18.67542&id=800">
+			<img src="_launcher/airly.webp"/>
+			<p>Airly</p>
+		</a>
+		<a href="http://www.pogodynka.pl/polska/zory_zory">
+			<p>Pogodynka.pl</p>
+		</a>
+		<a href="https://www.endomondo.com/home">
+			<img src="_launcher/endomondo.jpg" />
+			<p>Endomondo</p>
+		</a>
+	</div>
+
+	<div><h3>Talks</h3>
+		<a href="https://www.messenger.com/">
+			<img src="_launcher/messenger.png"/>
+			<p>Messenger</p>
+		</a>
+		<a href="https://disqus.com/home/inbox/">
+			<img src="_launcher/disqus.png"/>
+			<p>Disqus</p>
+		</a>
+		<a href="https://messages.android.com/">
+			<img src="_launcher/messages.png"/>
+			<p>Messages for web</p>
+		</a>
+	</div>`
+
+$('#launcher').append(WebLauncher);
---
+++ card-5.js
@@ -0,0 +1,33 @@
+const card5Content = `
+<header>
+
+	<h2>Green Card</h2>
+	<p>Veggies</p>
+	
+</header>
+
+
+<section class="content">
+			
+	<h1>Dev</h1>
+	
+</section>
+
+
+<footer>
+	
+	<time>
+		<p></p>
+		<p></p>
+	</time>
+	
+</footer>
+
+
+
+<nav>
+	<a id="card_close" name-SVGKit="svg_close">Close</a>
+</nav>
+`
+
+$('#card-5-section').prepend(card5Content);
